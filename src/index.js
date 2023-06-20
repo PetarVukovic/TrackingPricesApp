64054222423d1c313662b0a5632d96b8dfaaa975
Petar Vukovic <petar.vukovic@adnet.hr>
2023-06-20 13:03:54 +0200

    51

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+Importamo reactDom objekt iz thrid part library
+*/
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
