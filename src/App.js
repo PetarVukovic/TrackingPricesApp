d20738263f55ffc6b34dbad60798faee0a340ef5
Petar Vukovic <petar.vukovic@adnet.hr>
2023-06-20 14:19:49 +0200

    PrqacticeComponenetsProps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import ExpensionItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+
+
 function App() {
   const expenses = [
     {
@@ -24,27 +26,8 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpensionItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-       <ExpensionItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-         <ExpensionItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-         <ExpensionItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <h2 h2>Lets get started!</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
