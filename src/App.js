c60d4c5ebdc717795794ea177552a925fb519f77
Petar Vukovic <petar.vukovic@adnet.hr>
2023-06-21 11:38:56 +0200

    End of section 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
 
-function App() {
+const App=() =>{
   const expenses = [
     {
       title: "Toilet paper",
@@ -30,6 +31,10 @@ function App() {
       <Expenses items={expenses}/>
     </div>
   );
+  /* return React.createElement('div', {}, 
+   React.createElement('h2',{}, 'Lets get started!'),
+   React.createElement(Expenses,{items: expenses}),
+   );*/
 }
 
 export default App;
